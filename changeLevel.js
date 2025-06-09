31b821934d76477473a483e6c35525a3a6f44f5f
Sid-Tek <sidtekumalla@gmail.com>
2025-06-09 11:11:38 -0400

    Clean up level exit placements to match with original exits

--- changeLevel.js
+++ changeLevel.js
@@ -6,22 +6,26 @@ function changeLevel(sphere, material) {
     if (currentSrc === "01") {
         teleportPlayer(0,0);
         sphere.setAttribute("material", "src: #02; side: back");
-        teleportLevelExit(0,20) // set exit location for NEXT level
+        teleportLevelExit(-17,88); // exit for LEVEL 2
     }
     else if (currentSrc === "02") {
         teleportPlayer(0,0);
         sphere.setAttribute("material", "src: #03; side: back");
+        teleportLevelExit(80,-30); // exit for LEVEL 3
     }
     else if (currentSrc === "03") {
         teleportPlayer(0,0);
         sphere.setAttribute("material", "src: #04; side: back");
+        teleportLevelExit(-64,55); // exit for LEVEL 4
     }
     else if (currentSrc === "04") {
         teleportPlayer(0,0);
         sphere.setAttribute("material", "src: #05; side: back");
+        teleportLevelExit(86,-26); // exit for LEVEL 5
     }
     else if (currentSrc === "05") {
         teleportPlayer(0,0);
         sphere.setAttribute("material", "src: #01; side: back");
+        teleportLevelExit(-75,-55); // exit for LEVEL 1
     }
 }
