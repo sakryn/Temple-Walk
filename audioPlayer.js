865bf94c91a2be9ca159b381f6c752b45c599af1
Sid-Tek <sidtekumalla@gmail.com>
2025-06-11 11:03:24 -0400

    -Add Hiro -Format code

--- audioPlayer.js
+++ audioPlayer.js
@@ -1,69 +1,69 @@
 window.addEventListener("DOMContentLoaded", function () {
-  const audio_01 = document.getElementById("Audio_01");
-  const audio_02 = document.getElementById("Audio_02");
-  const audio_03 = document.getElementById("Audio_03");
-  const audio_04 = document.getElementById("Audio_04");
-  const audio_05 = document.getElementById("Audio_05");
-  const audio_06 = document.getElementById("Audio_06");
-  const audio_07 = document.getElementById("Audio_07");
-  const audio_08 = document.getElementById("Audio_08");
-  const audio_09 = document.getElementById("Audio_09");
-  const audio_10 = document.getElementById("Audio_10");
-  const audio_11 = document.getElementById("Audio_11");
-  const audio_12 = document.getElementById("Audio_12");
-  const audio_13 = document.getElementById("Audio_13");
-  const audio_14 = document.getElementById("Audio_14");
-  const audio_15 = document.getElementById("Audio_15");
-  const audio_16 = document.getElementById("Audio_16");
-  const audio_17 = document.getElementById("Audio_17");
-  const audio_18 = document.getElementById("Audio_18");
+    const audio_01 = document.getElementById("Audio_01");
+    const audio_02 = document.getElementById("Audio_02");
+    const audio_03 = document.getElementById("Audio_03");
+    const audio_04 = document.getElementById("Audio_04");
+    const audio_05 = document.getElementById("Audio_05");
+    const audio_06 = document.getElementById("Audio_06");
+    const audio_07 = document.getElementById("Audio_07");
+    const audio_08 = document.getElementById("Audio_08");
+    const audio_09 = document.getElementById("Audio_09");
+    const audio_10 = document.getElementById("Audio_10");
+    const audio_11 = document.getElementById("Audio_11");
+    const audio_12 = document.getElementById("Audio_12");
+    const audio_13 = document.getElementById("Audio_13");
+    const audio_14 = document.getElementById("Audio_14");
+    const audio_15 = document.getElementById("Audio_15");
+    const audio_16 = document.getElementById("Audio_16");
+    const audio_17 = document.getElementById("Audio_17");
+    const audio_18 = document.getElementById("Audio_18");
 
 
-  window.addEventListener("keydown", function (e) {
-    if (e.key === "1") {
-        audio_01.play();
-        audio_01.onended = function () { audio_02.play(); };
-        audio_02.onended = function () { audio_03.play(); };
-        audio_03.onended = function () { window.exitEnabled = true; };
-    }
+    window.addEventListener("keydown", function (e) {
+        if (e.key === "1") {
+            audio_01.play();
+            audio_01.onended = function () { audio_02.play(); };
+            audio_02.onended = function () { audio_03.play(); };
+            audio_03.onended = function () { window.exitEnabled = true; };
+        }
 
 
-    if (e.key === "2") {
-        audio_04.play();
-        audio_04.onended = function () { audio_05.play(); };
-        audio_05.onended = function () { audio_06.play(); };
-        // After 6, wait 15 seconds
-        audio_06.onended = function () { audio_07.play(); };
-        audio_07.onended = function () { window.exitEnabled = true; };
-    }
+        if (e.key === "2") {
+            audio_04.play();
+            audio_04.onended = function () { audio_05.play(); };
+            audio_05.onended = function () { audio_06.play(); };
+            // After 6, wait 15 seconds
+            audio_06.onended = function () { audio_07.play(); };
+            audio_07.onended = function () { window.exitEnabled = true; };
+        }
 
 
-    if (e.key === "3") {
-        audio_08.play();
-        audio_08.onended = function () { audio_09.play(); };
-        // After 9, wait 15 seconds
-        audio_09.onended = function () { audio_10.play(); };
-        audio_10.onended = function () { audio_11.play(); };
-        audio_10.onended = function () { window.exitEnabled = true; };
-    }
+        if (e.key === "3") {
+            audio_08.play();
+            audio_08.onended = function () { audio_09.play(); };
+            // After 9, wait 15 seconds
+            audio_09.onended = function () { audio_10.play(); };
+            audio_10.onended = function () { audio_11.play(); };
+            audio_10.onended = function () { window.exitEnabled = true; };
+        }
 
 
-    if (e.key === "4") {
-        audio_12.play();
-        audio_12.onended = function () { audio_13.play(); };
-        audio_13.onended = function () { window.exitEnabled = true; };
-    }
+        if (e.key === "4") {
+            audio_12.play();
+            audio_12.onended = function () { audio_13.play(); };
+            audio_13.onended = function () { window.exitEnabled = true; };
+        }
 
 
-    if (e.key === "5") {
-        audio_14.play();
-        audio_14.onended = function () { audio_15.play(); };
-        audio_15.onended = function () { audio_16.play(); };
-        audio_16.onended = function () { audio_17.play(); };
-        audio_17.onended = function () { audio_18.play(); };
-        audio_18.onended = function () { window.exitEnabled = true; };
-    }
+        if (e.key === "5") {
+            audio_14.play();
+            audio_14.onended = function () { audio_15.play(); };
+            audio_15.onended = function () { audio_16.play(); };
+            audio_16.onended = function () { audio_17.play(); };
+            audio_17.onended = function () { audio_18.play(); };
+            audio_18.onended = function () { window.exitEnabled = true; };
+        }
 
 
-  });
+    });
 });
--- changeLevel.js
+++ changeLevel.js
@@ -4,44 +4,45 @@ function changeLevel(sphere, material) {
 
     if (currentSrc === "01") {
         sphere.setAttribute("material", "src: #02; side: back");
-        teleportLevelExit(-17,88); // exit for LEVEL 2
+        teleportLevelExit(-17, 88); // exit for LEVEL 2
         setTimeout(() => {
-            teleportPlayer(0,0);
+            teleportPlayer(0, 0);
             changeExit(currentSrc);
         }, 1);
     }
     else if (currentSrc === "02") {
-        
+
         sphere.setAttribute("material", "src: #03; side: back");
-        teleportLevelExit(80,-30); // exit for LEVEL 3
+        teleportLevelExit(80, -30); // exit for LEVEL 3
         setTimeout(() => {
-            teleportPlayer(0,0);
+            teleportPlayer(0, 0);
             changeExit(currentSrc);
-        }, 1);    }
+        }, 1);
+    }
     else if (currentSrc === "03") {
-        
+
         sphere.setAttribute("material", "src: #04; side: back");
-        teleportLevelExit(-64,55); // exit for LEVEL 4
+        teleportLevelExit(-64, 55); // exit for LEVEL 4
         setTimeout(() => {
-            teleportPlayer(0,0);
+            teleportPlayer(0, 0);
             changeExit(currentSrc);
         }, 1);
     }
     else if (currentSrc === "04") {
-        
+
         sphere.setAttribute("material", "src: #05; side: back");
-        teleportLevelExit(86,-26); // exit for LEVEL 5
+        teleportLevelExit(86, -26); // exit for LEVEL 5
         setTimeout(() => {
-            teleportPlayer(0,0);
+            teleportPlayer(0, 0);
             changeExit(currentSrc);
         }, 1);
     }
     else if (currentSrc === "05") {
-        
+
         sphere.setAttribute("material", "src: #01; side: back");
-        teleportLevelExit(-75,-55); // exit for LEVEL 1
+        teleportLevelExit(-75, -55); // exit for LEVEL 1
         setTimeout(() => {
-            teleportPlayer(0,0);
+            teleportPlayer(0, 0);
             changeExit(currentSrc);
         }, 1);
     }
