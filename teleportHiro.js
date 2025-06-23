c2afd7cb5973bbef8faffcdd38a20f81d8a8073a
Sid-Tek <sidtekumalla@gmail.com>
2025-06-23 12:50:30 -0400

    Lowered Hiro and increased his size, specifically his height

--- teleportHiro.js
+++ teleportHiro.js
@@ -4,3 +4,10 @@ function teleportHiro(x, z) {
     hiro.setAttribute("position", { x: x, y: -0.5, z: z });
 
 }
+
+function teleportHiro(x, y, z) {
+    const hiro = document.querySelector("#hiro");
+
+    hiro.setAttribute("position", { x: x, y:y, z: z });
+
+}
