dc653b19feda6638b977173a60e5cbbad507aeb3
Sid-Tek <sidtekumalla@gmail.com>
2025-06-23 13:11:31 -0400

    edited the bowl to be larger as well

--- teleportBowl.js
+++ teleportBowl.js
@@ -4,3 +4,10 @@ function teleportBowl(x, z) {
     bowl.setAttribute("position", { x: x, y: 0, z: z })
 
 }
+
+function teleportBowl(x, y, z) {
+    const bowl = document.querySelector("#bowl")
+
+    bowl.setAttribute("position", { x: x, y: y, z: z })
+
+}
