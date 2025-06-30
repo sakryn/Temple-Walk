00806ae9ff718c17cde17028cc40662c4ca19551
Sid-Tek <sidtekumalla@gmail.com>
2025-06-30 10:47:04 -0400

    Raised the bowl by 20 units so that it is more visible to the player

--- teleportBowl.js
+++ teleportBowl.js
@@ -4,13 +4,13 @@ function teleportBowl(x, z, y) {
         window.bowl.setAttribute("position", { x: x, y: y, z: z });
     } else if (typeof x === "number" && typeof z === "number") {
         // Only x and z provided, use default y
-        window.bowl.setAttribute("position", { x: x, y: -60, z: z });
+        window.bowl.setAttribute("position", { x: x, y: -40, z: z });
     } else {
         // No coordinates provided, use hiro's position
         const hiroPosition = window.hiro.getAttribute("position");
         window.bowl.setAttribute("position", {
             x: hiroPosition.x + 30,
-            y: -60,
+            y: -40,
             z: hiroPosition.z + 30
         });
     }
